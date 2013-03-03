4b513bb0a5ca271a2d54cb3c6d52546f62c36caf
Vin <bevin@jabana.se>
2013-03-03 22:50:57 +0100

    Added font map, tried to print to screen

--- tacticsCore.c
+++ tacticsCore.c
@@ -5,26 +5,40 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include <uzebox.h>
+#include "defines.h"
 
 
 /* data includes */
 
-
+#include "res/fontmap.inc"
 
 
 /* globals */
 
 
 
+/* declarations */
+void initialize();
 
 
 
 
 /* main function */
 int main() {
+	initialize();
 
+	Print(0,0,PSTR("Hello world!"));
 
 	return 0;
+}
+
+
+void initialize() {
+	ClearVram();
+	SetTileTable(font_map);
+	SetFontTilesIndex(0);
+
+
 }
 
 
