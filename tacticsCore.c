67e6b462a4fef4ae05c4e580ca5dfdacd473adfd
Vin <bevin@jabana.se>
2013-03-05 17:08:25 +0100

    Added some globals and a loadLevel method

--- tacticsCore.c
+++ tacticsCore.c
@@ -5,7 +5,6 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include <uzebox.h>
-#include "defines.h"
 
 
 /* data includes */
@@ -13,25 +12,53 @@
 #include "res/tiles.inc"
 #include "res/fontmap.inc"
 
+/* structs */
+struct GridBufferSquare {
+    char terrain;
+    char owner;
+    char unit; // 0xff for no unit
+};
+struct Unit {
+    char inUse;
+    char type;
+    char player;
+    char hp;
+    char xPos;
+    char yPos;
+};
+
+
 /* globals */
+char levelWidth, levelHeight;
+
+// the complete level definition
+struct GridBufferSquare levelBuffer[40][14]; // the max level width has to be hard coded
+
+// what is visible on the screen; 15 wide, 14 high, 2 loading columns on each side
+struct GridBufferSquare screenBuffer[17][14]; // should this be pointers to levelBuffer items, perhaps?
+
+struct Unit unitList[40]; //is this enough?
 
 /* defines */
-#define PL1	0b10000000
-#define PL2	0b01000000
-#define NEU	0b00000000
+#define PL1	0x80
+#define PL2	0x40
+#define NEU	0x00
 
-#define PL	0x1 // plain
-#define MO	0x2 // mountain
-#define FO	0x3 // forest
-#define CT	0x4 // city
-#define BS	0x5 // base
+#define PL	0x01 // plain
+#define MO	0x02 // mountain
+#define FO	0x03 // forest
+#define CT	0x04 // city
+#define BS	0x05 // base
 
 #define TERRAIN_MASK 0b00000111
 #define UNIT_MASK	 0b00111000
 #define OWNER_MASK	 0b11000000
 
 /* declarations */
+// param1, param2, param3; return
 void initialize();
+void loadLevel(const char*); // level
+char addUnit(char, char, char, char); // x, y, player, type; unitIndex
 
 const char testlevel[] PROGMEM =
 {
@@ -64,7 +91,6 @@ void main() {
 			{
 				case PL:
 					DrawMap2(2*x, 2*y, map_plain);
-
 					break;
 				case MO:
 					DrawMap2(2*x, 2*y, map_mountain);
@@ -122,4 +148,44 @@ void initialize() {
 	ClearVram();
 	SetFontTilesIndex(TERRAINTILES_SIZE);
 	SetTileTable(terrainTiles);
+}
+
+void loadLevel(const char* level) {
+	char val, terr, owner, unit;
+	int x, y; // i know i said this wasn't needed but there will be overflow on the array access otherwise
+
+	levelWidth = pgm_read_byte(&level[0]);
+	levelHeight = 14;
+
+	/*
+	if(levelWidth >= 40) {
+		//this shouldn't be allowed to happen
+	}
+	*/
+
+	// loop y first because then we work in order. locality probably isn't an issue but eh.
+	for(y = 0; y < levelHeight; y++) {
+		for(x = 0; x < levelWidth; x++) {
+			val = pgm_read_byte(&level[y*levelWidth+x+1]);
+			terr = val & TERRAIN_MASK;
+			owner = val & OWNER_MASK;
+			unit = val & UNIT_MASK;
+			if(unit != 0 && owner != NEU) {
+				//this can be a unit
+				switch(unit) {
+				// TODO: insert code to add a unit for that player on that square
+				// TODO: be sure to set the unit variable to the unit list index!
+				default:
+					break;
+				}
+			}
+			else {
+				unit = 0xff; // no unit
+			}
+			// TODO: would it be better to store terrain and owner in the same variable?
+			levelBuffer[x][y].terrain = terr;
+			levelBuffer[x][y].owner = owner;
+			levelBuffer[x][y].unit = unit;
+		}
+	}
 }
